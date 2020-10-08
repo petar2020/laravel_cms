88c0b73465b761185d4c9ecb62482014e1fcf2a0
petar2020 <petar.arsic14@hotmail.com>
2020-10-08 15:26:23 +0200

    dodavene korisnika u bazu i prikazivanje

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.admin')
+
+
+
+
+
+@section('content')
+
+
+
+    <h1>Users</h1>
+
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Status</th>
+            <th>Created</th>
+            <th>Updated</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @if($users)
+
+
+            @foreach($users as $user)
+
+
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td> <img height="50" src="{{'http://placehold.it/400x400'}}" alt="" ></td>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->role->name}}</td>
+                    <td>{{$user->is_active == 1 ? 'Active' : 'No Active'}}</td>
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                    <td>{{$user->updated_at->diffForHumans()}}</td>
+                </tr>
+
+            @endforeach
+
+
+        @endif
+
+
+        </tbody>
+    </table>
+
+
+@stop
