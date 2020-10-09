ed6a5ad42903b47b8acefa1304236ba368e5b5a9
petar2020 <petar.arsic14@hotmail.com>
2020-10-09 15:22:30 +0200

    dodavanje slika, update profil

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\User;
@@ -46,7 +47,13 @@ class AdminUsersController extends Controller
     {
         //
 //     return $request->all();
-           $input = $request->all();
+        if(trim($request->password) == ''){
+            $input = $request->except('password');
+        } else {
+            $input = $request->all();
+            $input['password']= bcrypt($request->password);
+        }
+
 
             if($file = $request->file('photo_id')) {
 
@@ -89,6 +96,10 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
+        $roles = Role::pluck('name', 'id')->all();
+        $user = User::findOrFail($id);
+
+        return view('admin.users.edit', compact('user', 'roles'));
     }
 
     /**
@@ -98,10 +109,35 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest   $request, $id)
     {
-        //
-    }
+
+        $user = User::findorFail($id);
+        if(trim($request->password) == ''){
+            $input = $request->except('password');
+        } else {
+            $input = $request->all();
+            $input['password']= bcrypt($request->password);
+        }
+
+
+
+        if($file = $request->file('photo_id')){
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id']= $photo->id;
+
+
+        }
+        $user->update($input);
+
+        return redirect('/admin/users');
+        }
 
     /**
      * Remove the specified resource from storage.
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -34,10 +34,10 @@
 
                 <tr>
                     <td>{{$user->id}}</td>
-                    <td> <img height="50" src="{{'http://placehold.it/400x400'}}" alt="" ></td>
-                    <td>{{$user->name}}</td>
+                    <td><img height="50px" width="65px" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt=""></td>
+                    <td><a href="{{route('users.edit', $user->id)}}">{{$user->name}}</a></td>
                     <td>{{$user->email}}</td>
-                    <td>{{$user->role->name}}</td>
+                    <td>{{$user->role ? $user->role->name : 'User has no role'}}</td>
                     <td>{{$user->is_active == 1 ? 'Active' : 'No Active'}}</td>
                     <td>{{$user->created_at->diffForHumans()}}</td>
                     <td>{{$user->updated_at->diffForHumans()}}</td>
